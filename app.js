08aec6068517a0d8b315ca93f95fe6ac025c5ed6
mugisha isaie <mugishaisaieog1@gmail.com>
2025-07-22 06:02:38 +0200

    starting express

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
-// Events
+const app = require('http');
 
-const EventEmitter = require('events');
-const customEventEmitter = new EventEmitter();
+const {readFileSync} = require('fs');
+const server = app.createServer((request,response)=>{
+    // console.log(request.url)
+    const HomePage = readFileSync('./navbar-app/index.html');
+    const HomeStyle = readFileSync('./navbar-app/styles.css');
+    const HomeLogo = readFileSync('./navbar-app/logo.svg');
+    const HomeLogic = readFileSync('./navbar-app/browser-app.js');
 
-customEventEmitter.on("kwiga",()=>{
-    console.log("This is event 1");
+    const url = request.url;
+    if(url === '/'){
+        response.writeHead(200,{'content-type':'text/html'})
+        response.write(HomePage);
+        response.end();
+
+    }else if(url === '/about'){
+        response.writeHead(200,{'content-type':'text/html'})
+        response.write('<h1>About Page</h1>');
+        response.end();
+    
+    }else if (url === '/styles.css'){
+        response.writeHead(200,{'content-type':'text/css'})
+        response.write(HomeStyle);
+        response.end();
+    }else if (url === '/logo.svg'){
+        response.writeHead(200,{'content-type':'image/svg+xml'})
+        response.write(HomeLogo);
+        response.end();
+    }else if (url === '/browser-app.js'){
+        response.writeHead(200,{'content-type':'text/javascript'})
+        response.write(HomeLogic);
+        response.end();
+    }else{
+        response.writeHead(200,{'content-type':'text/html'})
+        response.write('<h1>Page Not Found</h1>');
+        response.end();
+    }
+
 })
 
-
-customEventEmitter.emit("kwiga");
+server.listen(3000)
